8bc17be71a3e663288de68ddf732094647be6a52
Vinsengi <nvip2020@gmail.com>
2025-05-03 16:06:51 +0200

    Create Procfile, get ready for heroku deployment

--- hello_world_app/views.py
+++ hello_world_app/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse # type: ignore
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey World, Am learning Django :)")
-    
+    if request.method == "POST":
+        return HttpResponse("Hey, You must have POSTed something :)")
+    else:
+        return HttpResponse(f"Hey, this was a {request.method} request Method")
--- my_1_blog/urls.py
+++ my_1_blog/urls.py
@@ -21,6 +21,6 @@ from about import views as about_views
 
 urlpatterns = [
     path('about/', about_views.about_me, name="about"),
-    path('hello/', index_view.index, name="index"),
+    path('', index_view.index, name="index"),
     path('admin/', admin.site.urls),
 ]
