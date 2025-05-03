66e901d626b35663e78024ad13c24f841da0c3c1
Vinsengi <nvip2020@gmail.com>
2025-05-03 14:09:23 +0200

    Create first app

---
+++ hello_world_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render # type: ignore
+from django.http import HttpResponse # type: ignore
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hey World, Am learning Django :)")
+    
